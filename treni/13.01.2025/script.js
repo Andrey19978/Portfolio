c50bf9b88be72637037ba310b803293fdc0b676d
Andrey19978 <135252366+Andrey19978@users.noreply.github.com>
2025-01-19 19:48:14 +0300

    d

--- treni/13.01.2025/script.js
+++ treni/13.01.2025/script.js
@@ -1,5 +1,23 @@
-function sayHi() {
-    alert('Привет');
-  }
+// Создаем объект group
+let group = {
+  // Свойство title
+  title: "Our Group",
   
-  setTimeout(sayHi, 10000);
+  // Свойство students - массив строк
+  students: ["John", "Pete", "Alice"],
+  
+  // Метод showList
+  showList() {
+    // Используем метод forEach для массива students
+    this.students.forEach(
+      // Функция для каждого элемента в массиве
+      student => {
+        // Выводим сообщение с названием группы и именем студента
+        alert(this.title + ': ' + student)
+      }
+    );
+  }
+};
+
+// Вызываем метод showList
+group.showList();
